16cad7709e60f64b5db2dc1bb6a2ba03d56b0e64
yashgupta31 <yashsantoshgupta2019@gmail.com>
2025-07-24 17:45:50 +0530

    onbording form

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Onboarding from './Pages/onboarding/Onboarding'
+import OnboardingForm from './Pages/onboarding/OnboardingForm'
+
+function App() {
+
+  return (
+     <div className="min-h-screen bg-gradient-to-br from-blue-50 via-indigo-50 to-purple-50">
+      <OnboardingForm />
+    </div>
+  )
+}
+
+export default App
